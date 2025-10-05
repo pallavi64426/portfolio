bc89d1fd6e6b789d1213bff8a89618a9ee80e091
pallavi64426 <pallavi6442685@gmail.com>
2025-10-05 19:48:26 +0530

    Added Flutter web build

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -22,7 +22,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "8be1720285b9a9e9f6e095281ea8a062",
+"flutter_bootstrap.js": "ac156745871962769dff18a0510e4a30",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
